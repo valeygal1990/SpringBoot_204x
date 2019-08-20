42998b83019a2ce78aba77c9cb0df63d67b740dd
valeygal1990 <valeygal360@gmail.com>
2019-08-20 16:39:26 -0400

    SpringBoot_204_Exercise

---
+++ src/main/java/com/example/demo/SpringBootCarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCarApplication.class, args);
+    }
+
+}
